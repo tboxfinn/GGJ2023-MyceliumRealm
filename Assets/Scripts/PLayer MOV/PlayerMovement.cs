2c7124726f4779bcca826ecd9bc1e89892af9bb5
HugoVD <sartos3000@hotmail.es>
2023-01-31 13:52:36 -0600

    MAP START

--- Assets/Scripts/PLayer MOV/PlayerMovement.cs
+++ Assets/Scripts/PLayer MOV/PlayerMovement.cs
@@ -9,13 +9,22 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D playerRb;
 
     private Vector2 moveDirection;
-    public bool DodgeRoll;
-    public float DodgeSpeed,DodgeTime;
-    
 
+    [Header("ProyectilesToSpawn")]
+    public GameObject proyectil1Prefab;
+
+
+    [SerializeField] private float activeMoveSpeed;
+    public float dashSpeed;
+
+    public float dashLenght, dashCooldown;
+
+    [SerializeField] private float dashCounter;
+    [SerializeField] private float dashCoolCounter;
 
     void Start()
     {
+        activeMoveSpeed = moveSpeed;
         playerRb = GetComponent<Rigidbody2D>();
     }
 
@@ -24,6 +33,39 @@ public class PlayerMovement : MonoBehaviour
     {
         //INPUTS
         ProcessInputs();
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("HOLA");
+
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed= dashSpeed;
+                dashCounter = dashLenght;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if(dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if(dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
     }
 
     void ProcessInputs()
@@ -32,18 +74,26 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+        
+        moveDirection.Normalize();
     }
-
 
     void FixedUpdate()
     {
         //CALCULAR FISICAS
         Move();
-    }
 
+         
+    }
 
     void Move()
     {
-        playerRb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        playerRb.velocity = new Vector2(moveDirection.x * activeMoveSpeed, moveDirection.y * activeMoveSpeed);
     }
+    
+    public void Shoot()
+    {
+        
+    }
+    
 }
