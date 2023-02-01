103c0cbf75fbec6f06a1a5f9f85f9b14486ded64
Hectorzilla <hectortra123@gmail.com>
2023-02-01 12:01:30 -0600

    lo que falta
    nomas que falto unas cosas que tienen que ver con cuando mueres y ya

--- Assets/change.cs
+++ Assets/change.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class change : MonoBehaviour
 {
     // Start is called before the first frame update
     public void Scene1()
     {
-        SceneManager.LoadScene("¨Menu");
+        SceneManager.LoadScene("Menu");
     }
     public void Scene2()
     {
         SceneManager.LoadScene("Main");
     }
+    public GameObject lol;
+
+    public void pausa()
+    {
+        Time.timeScale = 0;
+        lol.SetActive(true);
+    }
+    public void despausa()
+    {
+        lol.SetActive(false);
+        Time.timeScale = 1;
+    }
+
 }
